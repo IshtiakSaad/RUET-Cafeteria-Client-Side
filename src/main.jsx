9691aa9d2261289ddfb5001df39e081e46d3c155
Ishtiak Saad <ishtiak.m.saad@gmail.com>
2025-01-22 00:33:09 +0600

    Navbar and Footer done

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import './index.css'; 
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 
--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,11 +1,130 @@
-import React from 'react';
+import { useState } from "react";
 
-const Navbar = () => {
-    return (
-        <div>
-            
+const Navbar = ({ user }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  return (
+    <div className="navbar bg-base-100 shadow">
+      {/* Navbar Start */}
+      <div className="navbar-start">
+        {/* Mobile Dropdown */}
+        <div className="dropdown">
+          <button
+            tabIndex={0}
+            className="btn btn-ghost lg:hidden"
+            onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </button>
+          {isDropdownOpen && (
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a href="/">Home</a>
+              </li>
+              <li>
+                <a href="/meals">Meals</a>
+              </li>
+              <li>
+                <a href="/upcoming-meals">Upcoming Meals</a>
+              </li>
+            </ul>
+          )}
         </div>
-    );
+
+        {/* Logo */}
+        <a href="/" className="btn btn-ghost text-xl">
+          RUET Hall
+        </a>
+      </div>
+
+      {/* Navbar Center */}
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/meals">Meals</a>
+          </li>
+          <li>
+            <a href="/upcoming-meals">Upcoming Meals</a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Navbar End */}
+      <div className="navbar-end">
+        {/* Notification Icon */}
+        <button className="btn btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6 6 0 10-12 0v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0a3 3 0 11-6 0"
+            />
+          </svg>
+        </button>
+
+        {/* User Profile or Login */}
+        {user ? (
+          <div className="dropdown dropdown-end">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost btn-circle avatar"
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <div className="w-10 rounded-full">
+                <img src={user.profilePicture} alt="User Avatar" />
+              </div>
+            </label>
+            {isDropdownOpen && (
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-50 mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <span>{user.username}</span>
+                </li>
+                <li>
+                  <a href="/dashboard">Dashboard</a>
+                </li>
+                <li>
+                  <button onClick={user.logout}>Logout</button>
+                </li>
+              </ul>
+            )}
+          </div>
+        ) : (
+          <a href="/login" className="btn">
+            Login
+          </a>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
